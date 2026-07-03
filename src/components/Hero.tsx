ff8d56ebd632ca6d52804e0dcec6fa2afc8477de
Mrekanayake6 <lakshanekanayake6@gmail.com>
2026-07-04 00:25:47 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -128,7 +128,7 @@ export default function Hero() {
             initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ delay: 0.3, duration: 0.6 }}
           >
             {/* Spinning ring */}
-            <div className="relative" style={{ width: 350, height: 350 }}>
+            <div className="relative" style={{ width: 550, height: 550 }}>
               <motion.div
                 className="absolute inset-0 rounded-full"
                 style={{
